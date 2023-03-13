b24a0a1fb40ec40731ab67e44386876c322e2d53
guantanamobosch <luketyson17@gmail.com>
2023-03-13 09:42:58 -0600

    saving changes

--- controllers/media.js
+++ controllers/media.js
@@ -9,22 +9,22 @@ router.get('/', (req, res) => {
 // routing to TV shows only
 router.get('/tv', (req, res) => {
     res.render('/media/tv/index.ejs')
-})
+});
 
 // routing to specific tv show from the tv show index
 router.get('/tv/:id', (req, res) => {
     res.render('/media/tv/show.ejs')
-})
+});
 
 // routing to movies only
 router.get('/movies', (req, res) => {
     res.render('/media/movies/index.ejs')
-})
+});
 
 // routing to specific movie from the movie index
 router.get('/movies/:id', (req, res) => {
     res.render('/media/movies/show.ejs')
-})
+});
 
 // routing to show page for specific movie or TV from the general home page
 router.get('/:id', (req, res) => {

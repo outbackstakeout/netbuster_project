e8ba2226ea1be17e496c0f0d108cae80803335a9
guantanamobosch <luketyson17@gmail.com>
2023-03-13 08:47:20 -0600

    add routes to the user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('/user/index.ejs');
+});
+
+router.get('/:id', (req, res) => {
+    res.render('/user/show.ejs');
+});
+
 module.exports = router;
